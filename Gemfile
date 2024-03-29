8d4e892a4fa264369d9cb82ef9e7e42519b8bd4a
Dino Maric <dino.onex@gmail.com>
2024-03-29 15:40:35 +0100

    More

--- Gemfile
+++ Gemfile
@@ -66,4 +66,4 @@ group :test do
   gem 'selenium-webdriver'
 end
 
-gem 'hotwire_combobox'
+gem 'hotwire_combobox', github: 'josefarias/hotwire_combobox'
--- app/controllers/manifacturers_controller.rb
+++ app/controllers/manifacturers_controller.rb
@@ -1,14 +1,18 @@
 class ManifacturersController < ApplicationController
-  before_action :set_manifacturer, only: %i[ show edit update destroy ]
+  before_action :set_manifacturer, only: %i[show edit update destroy]
 
   # GET /manifacturers or /manifacturers.json
   def index
     @manifacturers = Manifacturer.all
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream
+    end
   end
 
   # GET /manifacturers/1 or /manifacturers/1.json
-  def show
-  end
+  def show; end
 
   # GET /manifacturers/new
   def new
@@ -16,8 +20,7 @@ class ManifacturersController < ApplicationController
   end
 
   # GET /manifacturers/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /manifacturers or /manifacturers.json
   def create
@@ -25,7 +28,7 @@ class ManifacturersController < ApplicationController
 
     respond_to do |format|
       if @manifacturer.save
-        format.html { redirect_to manifacturer_url(@manifacturer), notice: "Manifacturer was successfully created." }
+        format.html { redirect_to manifacturer_url(@manifacturer), notice: 'Manifacturer was successfully created.' }
         format.json { render :show, status: :created, location: @manifacturer }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +41,7 @@ class ManifacturersController < ApplicationController
   def update
     respond_to do |format|
       if @manifacturer.update(manifacturer_params)
-        format.html { redirect_to manifacturer_url(@manifacturer), notice: "Manifacturer was successfully updated." }
+        format.html { redirect_to manifacturer_url(@manifacturer), notice: 'Manifacturer was successfully updated.' }
         format.json { render :show, status: :ok, location: @manifacturer }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +55,20 @@ class ManifacturersController < ApplicationController
     @manifacturer.destroy!
 
     respond_to do |format|
-      format.html { redirect_to manifacturers_url, notice: "Manifacturer was successfully destroyed." }
+      format.html { redirect_to manifacturers_url, notice: 'Manifacturer was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_manifacturer
-      @manifacturer = Manifacturer.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def manifacturer_params
-      params.require(:manifacturer).permit(:name)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_manifacturer
+    @manifacturer = Manifacturer.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def manifacturer_params
+    params.require(:manifacturer).permit(:name)
+  end
 end
